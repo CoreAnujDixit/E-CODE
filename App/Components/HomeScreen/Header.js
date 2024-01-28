d9c901fae70f0e16e82b7524c70231c785998528
CoreAnujDixit <anujdixitdseu@gmail.com>
2024-01-28 11:51:01 +0530

    Ui Issue with bg, Dashboard name & img added for login

--- App/Components/HomeScreen/Header.js
+++ App/Components/HomeScreen/Header.js
@@ -1,17 +1,80 @@
-import { View, Image } from "react-native";
-import React from "react";
+import { View, Image, StyleSheet, Text } from "react-native";
+import React, { useState } from "react";
+import { useUser } from "@clerk/clerk-expo";
+import {
+  responsiveFontSize,
+  responsiveHeight,
+  responsiveWidth,
+} from "react-native-responsive-dimensions";
 
 export default function Header() {
+  const { isLoaded, isSignedIn, user } = useUser();
   return (
-    <View>
-      <Image
-        width={200}
-        height={200}
-        resizeMode="contain"
-        source={{
-          uri: "https://i.postimg.cc/FK3mHWHn/image.png",
-        }}
-      />
-    </View>
+    isLoaded && (
+      <View>
+        <View
+          style={{
+            padding: 20,
+            display: "flex",
+            flexDirection: "row",
+            gap: 10,
+            alignItems: "center",
+          }}
+        >
+          <Image
+            source={{ uri: user?.imageUrl }}
+            style={{
+              width: 60,
+              height: 60,
+              borderRadius: 99,
+              borderWidth: 2,
+              borderColor: "#fff",
+            }}
+          />
+          <View>
+            <Text style={{ fontFamily: "outfit", color: "white" }}>
+              Welcome
+            </Text>
+            <Text
+              style={{
+                fontFamily: "outfit-bold",
+                color: "#fff",
+                textShadowRadius: 6,
+                textShadowColor: "#000",
+                fontSize: responsiveFontSize(2.5),
+              }}
+            >
+              {user?.fullName}
+            </Text>
+          </View>
+        </View>
+        <View
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            gap: 10,
+            alignItems: "center",
+            padding: 25,
+          }}
+        >
+          <Image
+            source={require("../../../assets/coin.png")}
+            style={{
+              width: 35,
+              height: 35,
+            }}
+          />
+          <Text
+            style={{
+              fontFamily: "outfit-bold",
+              color: "#fff",
+              fontSize: responsiveFontSize(2),
+            }}
+          >
+            3588
+          </Text>
+        </View>
+      </View>
+    )
   );
 }
--- App/Screen/HomeScreen.js
+++ App/Screen/HomeScreen.js
@@ -1,11 +1,22 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import Header from '../Components/HomeScreen/Header'
+import { View, StyleSheet } from "react-native";
+import React from "react";
+import Header from "../Components/HomeScreen/Header";
+import { responsiveHeight } from "react-native-responsive-dimensions";
 
 export default function HomeScreen() {
-    return (
-        <View>
-            <Header />
-        </View>
-    )
+  return (
+    <View>
+      <Header />
+      <View style={styles.container}></View>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#DC84F3",
+    height: responsiveHeight(40),
+    bottom: responsiveHeight(25),
+    zIndex: -1,
+  },
+});
